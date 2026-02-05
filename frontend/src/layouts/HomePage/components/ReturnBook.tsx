4072bef9d2737f9d6afc74063cff6a0c8da6fb3b
ChuDucAnh242002 <anh.chu@tuni.fi>
2026-02-05 21:47:56 +0200

    Consume first API in the frontend

--- frontend/src/layouts/HomePage/components/ReturnBook.tsx
+++ frontend/src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,12 +1,18 @@
+import type BookModel from "../../../models/BookModel";
 import bookLuv2CodeImage from "./../../../Images/BooksImages/book-luv2code-1000.png";
 
-export const ReturnBook = () => {
+export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
   return (
     <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
       <div className="text-center">
-        <img src={bookLuv2CodeImage} width="151" height="233" alt="book" />
-        <h6 className="mt-2">Book</h6>
-        <p>Luv2Code</p>
+        {props.book.img ? (
+          <img src={props.book.img} width="151" height="233" alt="book" />
+        ) : (
+          <img src={bookLuv2CodeImage} width="151" height="233" alt="book" />
+        )}
+
+        <h6 className="mt-2">{props.book.title}</h6>
+        <p>{props.book.author}</p>
         <a className="btn main-color text-white" href="#">
           Reserve
         </a>
