d2625c29646ec5d775e89be54011f08c14b80415
ChuDucAnh242002 <anh.chu@tuni.fi>
2026-02-07 20:58:47 +0200

    Add React routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
 
 export const App = () => {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <HomePage />
+      <div className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/search" element={<SearchBooksPage />} />
+          <Route path="/checkout/:bookId" element={<BookCheckoutPage />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
